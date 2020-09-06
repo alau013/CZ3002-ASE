354d4e3ad058f38b486a5f70f67f38f16d37742f
alau013 <48979548+alau013@users.noreply.github.com>
2020-09-06 22:54:10 +0800

    lol

---
+++ gyroscope Test/Assets/followGryo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followGryo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Tweaks")]
+    [SerializeField] private Quaternion baseRotation = new Quaternion(0,0,1,0);
+    private void Start()
+    {
+        gyroManager.Instance.EnableGyro();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       transform.localRotation = gyroManager.Instance.GetGryoRotation() * baseRotation; 
+    }
+}
