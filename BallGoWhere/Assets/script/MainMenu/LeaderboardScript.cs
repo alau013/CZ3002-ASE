8a37c0092a48604a11057f6e2644787c2260d4e1
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-09-17 22:46:10 +0800

    Added skeleton leaderboard script.

---
+++ BallGoWhere/Assets/script/MainMenu/LeaderboardScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardScript : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("EntryContainer");
+        entryTemplate = entryContainer.Find("EntryTemplate");
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 60f;
+        for(int i = 0; i<10; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+        }
+        
+    }
+
+}
