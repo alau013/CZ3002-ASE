8196a193ba64220479286b7cb1a204f0fe1b0f40
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-09-28 15:52:07 +0800

    Update for scrollable username list at login and warning text

--- BallGoWhere/Assets/script/MainMenu/LoginMenu.cs
+++ BallGoWhere/Assets/script/MainMenu/LoginMenu.cs
@@ -10,9 +10,19 @@ public class LoginMenu : MonoBehaviour
     public GameObject LoginScreen;
     public GameObject MainScreen;
     public TMP_InputField username;
+    public TMP_Text LoginWarningText;
     public TMP_Text display; //from mainmenu
     public static Boolean loginSuccess = false;
     public static string playerName = null;
+
+    private Transform scrollView;
+    private Transform viewPort;
+
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private List<LoginEntry> loginEntryList;
+    private List<Transform> loginEntryTransformList;
+
     private void Awake()
     {
         if (loginSuccess)
@@ -20,6 +30,37 @@ public class LoginMenu : MonoBehaviour
             LoginScreen.SetActive(false);
             MainScreen.SetActive(true);
         }
+
+        else
+        {
+            this.LoginWarningText.text = "";
+            scrollView = transform.Find("Scroll View");
+            viewPort = scrollView.Find("Viewport");
+            entryContainer = viewPort.Find("Content");
+            entryTemplate = entryContainer.Find("EntryTemplate");
+
+            entryTemplate.gameObject.SetActive(false);
+
+            loginEntryList = new List<LoginEntry>()
+        {
+            new LoginEntry{name = "ahHuat10"},
+            new LoginEntry{name = "bond179"},
+            new LoginEntry{name = "doraemon"},
+        };
+
+            loginEntryTransformList = new List<Transform>();
+            foreach (LoginEntry loginEntry in loginEntryList)
+            {
+                CreateLoginEntryTransform(loginEntry, entryContainer, loginEntryTransformList);
+
+            }
+        }
+        
+    }
+
+    public void SetName(TMP_Text tmpName)
+    {
+        this.username.text = tmpName.text;
     }
     public void Login()
     {
@@ -42,7 +83,14 @@ public class LoginMenu : MonoBehaviour
             display.text = welcomeMsg;
             LoginScreen.SetActive(false);
             MainScreen.SetActive(true);
-            
+
+            //save successful username to playerPrefs
+
+        }
+
+        else
+        {
+            this.LoginWarningText.text = "Username already taken! Please try another...";
         }
     }
 
@@ -52,5 +100,26 @@ public class LoginMenu : MonoBehaviour
         LoginMenu.playerName = null;
     }
 
-   
+    //Represents a single leaderboard entry
+    private void CreateLoginEntryTransform(LoginEntry loginEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight = 60f;
+        //generate objects for each row
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        string name = loginEntry.name;
+
+        //set name for each row.
+        entryTransform.Find("nameButton").GetComponentInChildren<TMP_Text>().text = name;
+
+        transformList.Add(entryTransform);
+    }
+    private class LoginEntry
+    {
+        public string name;
+    }
+
 }
