a8e0d4b3a8c0f8850f5e1fbe163b624cebd256c8
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-09-28 15:11:24 +0800

    Update to add help information in Select Level Screen and codebehind for launching new scene/levels.

---
+++ BallGoWhere/Assets/script/MainMenu/SelectLevelScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class SelectLevelScript : MonoBehaviour
+{
+    public GameObject MenuScreen;
+    public GameObject SelectScreen;
+    public GameObject HelpScreen;
+    private bool helpToggle = false;
+
+    public void ToBack()
+    {
+        MenuScreen.SetActive(true);
+        SelectScreen.SetActive(false);
+    }
+
+    public void PlayLevel(string scene_name)
+    {
+        SelectScreen.SetActive(false);
+        SceneManager.LoadScene(scene_name);
+    }
+    public void ToHelp()
+    {
+        HelpScreen.SetActive(!helpToggle);
+        helpToggle = !helpToggle;
+    }
+}
