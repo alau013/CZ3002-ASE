8ef13babd4808a4eb07ed8bb3c03ab8fa5f7227f
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-10-02 15:01:58 +0800

    Update to load previous usernames from PlayerPrefs

--- BallGoWhere/Assets/script/MainMenu/LoginMenu.cs
+++ BallGoWhere/Assets/script/MainMenu/LoginMenu.cs
@@ -7,6 +7,7 @@ using System;
 
 public class LoginMenu : MonoBehaviour
 {
+    public GameObject PrefObject;
     public GameObject LoginScreen;
     public GameObject MainScreen;
     public TMP_InputField username;
@@ -22,8 +23,10 @@ public class LoginMenu : MonoBehaviour
     private Transform entryTemplate;
     private List<LoginEntry> loginEntryList;
     private List<Transform> loginEntryTransformList;
+    private PlayerPrefUI playerInfo;
+    private List<String> usernamesList;
 
-    private void Awake()
+    private void OnEnable() 
     {
         if (loginSuccess)
         {
@@ -41,22 +44,45 @@ public class LoginMenu : MonoBehaviour
 
             entryTemplate.gameObject.SetActive(false);
 
+            /*
             loginEntryList = new List<LoginEntry>()
         {
             new LoginEntry{name = "ahHuat10"},
             new LoginEntry{name = "bond179"},
             new LoginEntry{name = "doraemon"},
         };
-
-            loginEntryTransformList = new List<Transform>();
-            foreach (LoginEntry loginEntry in loginEntryList)
+        */
+            loginEntryList = new List<LoginEntry>();
+            playerInfo = PrefObject.GetComponent<PlayerPrefUI>();
+            usernamesList = playerInfo.availablePlayers();
+            if (usernamesList.Count > 0)
             {
-                CreateLoginEntryTransform(loginEntry, entryContainer, loginEntryTransformList);
-
+                Debug.Log("usernamesList Count: " + usernamesList.Count.ToString());
+                foreach (string username in usernamesList)
+                {
+                    loginEntryList.Add(new LoginEntry { name = username });
+                }
             }
+            
+            //test above
+            loginEntryTransformList = new List<Transform>();
+            if (loginEntryList.Count > 0)
+            {
+                Debug.Log("loginEntryList old Count: " + loginEntryList.Count);
+                ResetContent(entryContainer);
+                foreach (LoginEntry loginEntry in loginEntryList)
+                {
+                    CreateLoginEntryTransform(loginEntry, entryContainer, loginEntryTransformList);
+
+                }
+            }
+            
         }
         
+        
     }
+
+    
 
     public void SetName(TMP_Text tmpName)
     {
@@ -71,27 +97,56 @@ public class LoginMenu : MonoBehaviour
         
         Debug.Log(nameStr +" on device: "+ deviceID);
 
-        //Insert login authentication stuff
-
-        //After successful login, set display msg in MainScreen
-        LoginMenu.loginSuccess = true;
-        if (loginSuccess)
+        if (nameStr != "")
         {
-            LoginMenu.playerName = nameStr;
-            Debug.Log(playerName + "logged in");
-            string welcomeMsg = "Welcome " + playerName+"!";
-            display.text = welcomeMsg;
-            LoginScreen.SetActive(false);
-            MainScreen.SetActive(true);
+            //Insert login authentication stuff
+            //After successful login, set display msg in MainScreen
+            LoginMenu.loginSuccess = true;
+            if (loginSuccess)
+            {
+                //PlayerPrefs.DeleteAll(); //use this to reset PlayerPrefs when testing
+                playerInfo = PrefObject.GetComponent<PlayerPrefUI>();
+                usernamesList = playerInfo.availablePlayers();
 
-            //save successful username to playerPrefs
+                if (usernamesList.Count > 0)
+                {
+                    if (usernamesList.Contains(nameStr)) //load old username
+                    {
+                        playerInfo.Load(nameStr);
+                    }
+                    else //save new username
+                    {
+                        playerInfo.setUsername(nameStr);
+                        playerInfo.Save();
+                    }
+                }
+                else //save new username
+                {
+                    playerInfo.setUsername(nameStr);
+                    playerInfo.Save();
+                }
 
+
+                LoginMenu.playerName = nameStr;
+                Debug.Log(playerName + "logged in");
+                string welcomeMsg = "Welcome " + playerName + "!";
+                display.text = welcomeMsg;
+                LoginScreen.SetActive(false);
+                MainScreen.SetActive(true);
+
+
+            }
+
+            else
+            {
+                this.LoginWarningText.text = "Username already taken! Please try another...";
+            }
         }
-
         else
         {
-            this.LoginWarningText.text = "Username already taken! Please try another...";
+            this.LoginWarningText.text = "Please Enter A Username!";
         }
+        
     }
 
     public static void LogOut()
@@ -100,9 +155,25 @@ public class LoginMenu : MonoBehaviour
         LoginMenu.playerName = null;
     }
 
+    private void ResetContent(Transform entryContainer)
+    {
+        var children = new List<GameObject>();
+        foreach (Transform child in entryContainer) {
+            if(child.name != "EntryTemplate")
+            {
+                children.Add(child.gameObject);
+            }
+            
+
+         }
+        children.ForEach(child => Destroy(child));
+
+
+    }
     //Represents a single leaderboard entry
     private void CreateLoginEntryTransform(LoginEntry loginEntry, Transform container, List<Transform> transformList)
     {
+        Debug.Log("[CreateLoginEntryTransform] transformList count: " + transformList.Count.ToString());
         float templateHeight = 60f;
         //generate objects for each row
         Transform entryTransform = Instantiate(entryTemplate, container);
