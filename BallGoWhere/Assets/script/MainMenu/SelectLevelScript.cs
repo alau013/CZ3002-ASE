af6c9a9688b50d1c8edd4ad2f04eff379efdd387
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-10-10 23:33:02 +0800

    Update to switch between standard and special select levels.

--- BallGoWhere/Assets/script/MainMenu/SelectLevelScript.cs
+++ BallGoWhere/Assets/script/MainMenu/SelectLevelScript.cs
@@ -8,13 +8,34 @@ using System;
 
 public class SelectLevelScript : MonoBehaviour
 {
+    public Button TypeButton;
+    public GameObject SpecialScrollView;
+    public GameObject StandardScrollView; 
     public GameObject APIObject;
     public GameObject PrefObject;
     public GameObject MenuScreen;
     public GameObject SelectScreen;
     public GameObject HelpScreen;
     private bool helpToggle = false;
+    private int vizMode = 0; //0 - Standard, 1 - Special
+    private int numModes = 2;
 
+    public void OnEnable()
+    {
+        if(vizMode == 0)
+        {
+            TypeButton.GetComponentInChildren<TMP_Text>().text = "Standard";
+            StandardScrollView.SetActive(true);
+            SpecialScrollView.SetActive(false);
+
+        }
+        else
+        {
+            TypeButton.GetComponentInChildren<TMP_Text>().text = "Special";
+            StandardScrollView.SetActive(false);
+            SpecialScrollView.SetActive(true);
+        }
+    }
     public void ToBack()
     {
         MenuScreen.SetActive(true);
@@ -30,5 +51,19 @@ public class SelectLevelScript : MonoBehaviour
     {
         HelpScreen.SetActive(!helpToggle);
         helpToggle = !helpToggle;
+    }
+
+    public void togglePrevMode()
+    {
+        vizMode += 1;
+        vizMode %= numModes;
+        OnEnable();
+    }
+
+    public void toggleNextMode()
+    {
+        vizMode -= 1;
+        vizMode %= numModes;
+        OnEnable();
     }
 }
